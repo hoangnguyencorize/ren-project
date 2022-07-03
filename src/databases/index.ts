416a8907d547789f0def45f80fdf641f258b4c54
DESKTOP-M005LDE\HOANG <nguyentamhoang12a@gmail.com>
2022-07-03 15:37:14 +0700

    add auth mongo adminer

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,8 +1,8 @@
 import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
 
 export const dbConnection = {
-  // url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
-  url: 'mongodb+srv://root:root@cluster0.ce1zm.mongodb.net/dev?retryWrites=true&w=majority',
+  url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
+  // url: 'mongodb+srv://root:root@cluster0.ce1zm.mongodb.net/dev?retryWrites=true&w=majority',
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
